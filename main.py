6a93faa17daa5400ad92b643a21054201626352d
dark-200 <114500762+dark-200@users.noreply.github.com>
2024-02-28 14:46:04 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+name = "Baraa"
+age = 20
+address = "Nusa Putra Resedince Cisaat"
+hobbies = 'playing socer and watching moviesR'
+gender = 'MaleFaddress'
+
+print(f"My name is {name}\nMy age is {age} years old\nmy address is\nMy hobbies are{hobbies}\nMy gender is {gender}")
